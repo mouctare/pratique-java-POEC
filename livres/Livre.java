368a842d9f827d7766d442ce07715f2e1ff21412
mouctare <mouctard78@gmail.com>
2022-01-13 11:28:31 +0100

    getters et setters suite

--- livres/Livre.java
+++ livres/Livre.java
@@ -8,22 +8,20 @@ public class Livre {
     private String anneEdition;
     private String numeroISBN;
  
+    public Livre(String titre, String auteur){
+        this.titre = titre;
+        this.auteur = auteur;
+    }
 
    public Livre(String titre,String auteur, String anneEdition, String numeroISBN){
-         this.titre = titre;
-         this.auteur = auteur;
+        this(titre, auteur);
+        
          this.anneEdition = anneEdition;
          this.numeroISBN = numeroISBN;
 
    }
 
-   public void info(){
-    System.out.println("Je m'appelle " + auteur + " , " + "le titre de mon livre est " + titre + " il a été édité le " + anneEdition + " ," + " le numéro ISBN est le : " + numeroISBN);
-}
-   public void afficheTitreEtAuteur(){
-    System.out.println("Le titre du livre est "  + titre  + " il a été écrit par " + auteur);
-}
-  
+
 public String getTitre(){
     return titre;
 }
@@ -37,7 +35,7 @@ public void setAuteur(String auteur){
     this.auteur = auteur;
 }
 public String getAnneEdition(){
-    return auteur;
+    return anneEdition;
 }
 public void setAnneEdition(String anneEdition){
     this.anneEdition = anneEdition;
